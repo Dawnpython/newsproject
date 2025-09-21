10593ba53d5e1313dc668eb1684ca27c123cc88a
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-09-21 22:11:39 +0200

    quick commit

--- client/src/pages/userpage/Userpage.jsx
+++ client/src/pages/userpage/Userpage.jsx
@@ -2,13 +2,13 @@ import "/src/pages/userpage/Userpage.css";
 
 import Navbar from "../../components/navbar/Navbar";
 import Hero from "../../blocks/hero/Hero";
-import CategoryFirst from "/src/pages/userpage/Userpage.jsx";
+import CategoryFirst from "/src/blocks/categoryfirst/CategoryFirst.jsx";
 
 export default function Userpage() {
   return (
     <div className="userpage">
       <Hero />
-     
+      <CategoryFirst/>
       <Navbar />
     </div>
   );
