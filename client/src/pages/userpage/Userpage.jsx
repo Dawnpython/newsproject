2f87b3f80156d904dc43a806c296c1c92192c90d
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-09-21 22:47:27 +0200

    quick commit

--- client/src/pages/userpage/Userpage.jsx
+++ client/src/pages/userpage/Userpage.jsx
@@ -3,7 +3,7 @@ import "/src/pages/userpage/Userpage.css";
 import Navbar from "../../components/navbar/Navbar";
 import Hero from "../../blocks/hero/Hero";
 import CategoryFirst from "/src/blocks/categoryfirst/CategoryFirst.jsx";
-import Footer from "../../blocks/footer/footer";
+import Footer from "/src/blocks/footer/Footer.jsx";
 
 export default function Userpage() {
   return (
