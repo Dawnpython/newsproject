090138a316eb34c740294f32bdd02ba59628fa2f
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-09-21 22:46:42 +0200

    quick commit

---
+++ client/src/blocks/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import '/src/blocks/footer/Footer.css'
+
+export default function Footer(){
+    return(
+        <div className="footer">
+            <div className="footer-part-1">fs</div>
+            <div className="footer-part-2"></div>
+            <div className="footer-part-3"></div>
+        </div>
+    )
+}
--- client/src/pages/userpage/Userpage.jsx
+++ client/src/pages/userpage/Userpage.jsx
@@ -3,12 +3,14 @@ import "/src/pages/userpage/Userpage.css";
 import Navbar from "../../components/navbar/Navbar";
 import Hero from "../../blocks/hero/Hero";
 import CategoryFirst from "/src/blocks/categoryfirst/CategoryFirst.jsx";
+import Footer from "../../blocks/footer/footer";
 
 export default function Userpage() {
   return (
     <div className="userpage">
       <Hero />
       <CategoryFirst/>
+      <Footer/>
       <Navbar />
     </div>
   );
