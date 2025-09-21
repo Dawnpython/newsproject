4694d53b6ca329a1a44db1333c11e50e9fc44090
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-09-21 23:05:38 +0200

    quick commit

--- client/src/blocks/footer/Footer.jsx
+++ client/src/blocks/footer/Footer.jsx
@@ -1,11 +1,25 @@
-import '/src/blocks/footer/Footer.css'
+import "/src/blocks/footer/Footer.css";
+import logo from '/src/assets/logo.png'
 
-export default function Footer(){
-    return(
-        <div className="footer">
-            <div className="footer-part-1"></div>
-            <div className="footer-part-2"></div>
-            <div className="footer-part-3"></div>
-        </div>
-    )
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-part-1">
+        <h1>
+          О приложении
+          <br />
+          «В гостях к местным»
+        </h1>
+        <p>
+          Мы делаем путешествия по России и СНГ простыми и честными. Турист
+          получает всю нужную информацию в одном месте и напрямую связывается с
+          местными — гидами, владельцами отелей, таксистами, предпринимателями
+          организующими ваш отдых и досуг. <a>Читать далее</a>
+        </p>
+        <img src={logo}></img>
+      </div>
+      <div className="footer-part-2"></div>
+      <div className="footer-part-3"></div>
+    </div>
+  );
 }
--- client/src/utils/Imagestack.jsx
+++ client/src/utils/Imagestack.jsx
@@ -1,5 +1,6 @@
 import hero from '/src/assets/hero.png'
 import preloader from '/src/assets/First page 1.png'
 import bannerRequest from'/src/assets/img-banner-local.png'
+import logo from '/src/assets/logo.png'
 
-export const IMAGES_TO_PRELOAD = [preloader, hero,bannerRequest];
+export const IMAGES_TO_PRELOAD = [preloader, hero,bannerRequest,logo];
