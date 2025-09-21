b4df63099539b02ae421a77d0d68dd58a2702d27
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-09-21 22:49:37 +0200

    quick commit

--- client/src/blocks/footer/Footer.jsx
+++ client/src/blocks/footer/Footer.jsx
@@ -3,7 +3,7 @@ import '/src/blocks/footer/Footer.css'
 export default function Footer(){
     return(
         <div className="footer">
-            <div className="footer-part-1">fs</div>
+            <div className="footer-part-1"></div>
             <div className="footer-part-2"></div>
             <div className="footer-part-3"></div>
         </div>
