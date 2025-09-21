d3919eb3ae70d171f2ed81527886bbf2f224a67f
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-09-21 18:08:58 +0200

    quick commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 25000);
     return () => clearTimeout(timer);
   }, []);
 
