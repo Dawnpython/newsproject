3f2785c521339af2bd7b42a43c07ae83a268ae2b
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-09-25 21:17:44 +0200

    quick commit

--- client/src/components/fatcategory/Fatcategory.jsx
+++ client/src/components/fatcategory/Fatcategory.jsx
@@ -1,20 +1,28 @@
 import "/src/components/fatcategory/Fatcategory.css";
 
 import otelImg from "/src/assets/img-category-bg-3.png";
-import gidImg from "/src/assets/img-category-bg-6.png";
 import firstIcon from '/src/assets/icons/ fatcategory/img-category-1.svg'
+import secondIcon from '/src/assets/icons/ fatcategory/img-category-2.svg'
+import thirdIcon from '/src/assets/icons/ fatcategory/img-category-3.svg'
+import fourIcon from '/src/assets/icons/ fatcategory/img-category-4.svg'
+import fiveIcon from '/src/assets/icons/ fatcategory/img-category-5.svg'
+import sixIcon from '/src/assets/icons/ fatcategory/img-category-6.svg'
+import sevenIcon from '/src/assets/icons/ fatcategory/img-category-7.svg'
+import eightIcon from '/src/assets/icons/ fatcategory/img-category-8.svg'
+import nineIcon from '/src/assets/icons/ fatcategory/img-category-9.svg'
+import tenIcon from '/src/assets/icons/ fatcategory/img-category-10.svg'
 
 const DEFAULT_TILES = [
-  { label: "Лодки и паромы", bg: "linear-gradient(135deg,#0FB6FF,#00D586,#BEEF22)", icon: firstIcon },
-  { label: "Такси", bg: "#F3F4F6"},
-  { label: "Отели и турбазы", bg: "#1DDA94", bgImage: otelImg },
-  { label: "Где поесть", bg: "#FFDC4C" },
-  { label: "Маркетплейс", bg: "#7952EB" },
-  { label: "Гиды", color: "black", bg: "#d2d3d5ff", bgImage: gidImg },
-  { label: "Экскурсии", bg: "#27D9FE" },
-  { label: "Аренда жилья", bg: "linear-gradient(135deg,#FFC300,#FF8E00)" },
-  { label: "Магазины и рынки", bg: "#F3F4F6", color: "#222" },
-  { label: "Скидки и акции города", bg: "#FF0043" },
+  { label: "Лодки \nи паромы", bg: "linear-gradient(135deg,#0FB6FF,#00D586,#BEEF22)", icon: firstIcon },
+  { label: "Такси", bg: "#F3F4F6", icon: secondIcon,color:'black'},
+  { label: "Отели \nи турбазы", bg: "#1DDA94", bgImage: otelImg, icon:thirdIcon },
+  { label: "Где поесть", bg: "#FFDC4C", icon:fourIcon,color:'black' },
+  { label: "Маркетплейс", bg: "#7952EB", icon:fiveIcon },
+  { label: "Гиды", color: "black", bg: "#F3F4F6",  icon:sixIcon },
+  { label: "Экскурсии", bg: "#27D9FE", icon:sevenIcon },
+  { label: "Аренда \nжилья", bg: "linear-gradient(135deg,#FFC300,#FF8E00)", icon:eightIcon },
+  { label: "Магазины \nи рынки", bg: "#F3F4F6", color: "#222", icon:nineIcon },
+  { label: "Скидки и акции \nгорода", bg: "#FF0043",icon:tenIcon },
 ];
 
 export default function Fatcategory({ items = DEFAULT_TILES, onSelect }) {
