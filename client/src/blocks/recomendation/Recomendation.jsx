ea9ba677d341957e7ffb1c18db5a74210f3a0e31
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-09-27 14:07:53 +0200

    quick commit

--- client/src/blocks/recomendation/Recomendation.jsx
+++ client/src/blocks/recomendation/Recomendation.jsx
@@ -1,7 +1,8 @@
 import "/src/blocks/recomendation/Recomendation.css";
 
-import logrec from '/src/assets/ logrec.png'
-import instabanner from '/src/assets/img-inst-banner.png'
+import logrec from "/src/assets/ logrec.png";
+import instabanner from "/src/assets/img-inst-banner.png";
+import bannerlogo from "/src/assets/bannerlogo.png";
 
 import Stories from "../../components/stories/Stories";
 import Storiescat from "../../components/storiescat/Storiescat";
@@ -9,15 +10,17 @@ import Storiescat from "../../components/storiescat/Storiescat";
 export default function Recomendation() {
   return (
     <div className="recomendation">
-        <div className="rec-logo">
-            <img src={logrec}></img>
-        </div>
-        <Stories/>
-        <Storiescat/>
-        <div className="insta-banner">
-<img src={instabanner}></img>
- <p>Все статьи</p>
-        </div>
+      <div className="rec-logo">
+        <img src={logrec}></img>
+      </div>
+      <Stories />
+      <Storiescat />
+      <div className="insta-banner">
+        <img className="instalogo" src={bannerlogo}></img>
+        <img src={instabanner}></img>
+        <p>Все статьи</p>
+        <button>Смотреть всё</button>
+      </div>
     </div>
   );
 }
