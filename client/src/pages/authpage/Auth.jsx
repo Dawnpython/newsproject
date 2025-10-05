005929515c01ed3b935f76fd88b5126580ff7fcc
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-10-05 18:23:45 +0200

    quick commit

--- client/src/pages/authpage/Auth.jsx
+++ client/src/pages/authpage/Auth.jsx
@@ -1,11 +1,21 @@
-import '/src/pages/authpage/Auth.css'
-import Navbar from '/src/components/navbar/Navbar.jsx'
+import "/src/pages/authpage/Auth.css";
+import Navbar from "/src/components/navbar/Navbar.jsx";
 
 export default function Auth() {
-    return(
-        <>
-        <h1>hello</h1>
-        <Navbar/>
-        </>
-    )
+  return (
+    <div className="auth-page">
+      <h1>
+        Войдите или зарегистрируйтесь,
+        <br /> чтобы отправлять свои запросы
+        <br />
+        «местным»
+      </h1>
+      <div className="steps">
+        <div className="step"></div>
+        <div className="step"></div>
+        <div className="step"></div>
+      </div>
+      <Navbar />
+    </div>
+  );
 }
