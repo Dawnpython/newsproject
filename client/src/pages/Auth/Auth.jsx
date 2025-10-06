e401c5d93805d1f2c2ecffd166e7cde18e4b2d6f
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-10-06 07:10:07 +0200

    quick commit

---
+++ client/src/pages/Auth/Auth.jsx
@@ -0,0 +1,44 @@
+import "/src/pages/authpage/Auth.css";
+import Navbar from "/src/components/navbar/Navbar.jsx";
+
+import firstStep from '/src/assets/auth/1.png'
+import secondStep from '/src/assets/auth/2.png'
+
+export default function Auth() {
+  return (
+    <div className="auth-page">
+      <h1>
+        Войдите или зарегистрируйтесь,
+        <br /> чтобы отправлять свои запросы
+        <br />
+        «местным»
+      </h1>
+      <div className="steps">
+        <div className="step">
+            <div className="step-num">1</div>
+            <p>Вы пишите запрос,<br/>
+мы рассылаем<br/>
+его местным</p>
+             <img width={174} height={51} src={firstStep} ></img>
+        </div>
+        <div className="step">
+            <div className="step-num">2</div>
+            <p>Местные откликаются<br/>
+и предлагают вам <br/>
+свою помощь/услуги</p>
+<img width={150} height={75} src={secondStep} ></img>
+        </div>
+        <div className="step">
+            <div className="step-num">3</div>
+            <p>Выбираете лучшее<br/>
+предложение<br/>
+и связываетесь напрямую</p>
+        </div>
+      </div>
+      <p>Все просто, без суеты и поисков!</p>
+      <button className="authbut">Зарегистрироваться</button>
+      <button className="logbut">Войти</button>
+      <Navbar />
+    </div>
+  );
+}
