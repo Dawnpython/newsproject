50e26b3bb0ca5f47f7b56fa369a3981598c6d545
Yehor <yehor@MacBook-Pro-Yehor.local>
2025-10-06 20:51:34 +0200

    quick commit

--- client/src/pages/userprofile/Userprofile.jsx
+++ client/src/pages/userprofile/Userprofile.jsx
@@ -1,7 +1,13 @@
-export default function Userprofile(){
+export default function Userprofile(user, onLogout){
     return(
         <>
-        <h1>Hello</h1>
+        <div style={{ padding: 20 }}>
+      <h2>✅ Вы в аккаунте</h2>
+      <p><b>Имя:</b> {user?.name}</p>
+      <p><b>Email:</b> {user?.email}</p>
+      {user?.phone && <p><b>Телефон:</b> {user.phone}</p>}
+      <button onClick={onLogout} style={{ marginTop: 12 }}>Выйти</button>
+    </div>
         </>
     )
 }
